84dce0525f5337e8c66f51a20038ca8c4836649f
chiliwilys <siticay28@kurbieh.com>
2025-02-26 16:05:18 -0600

    Simple Animation

---
+++ script.js
@@ -0,0 +1,29 @@
+function jump(){
+    document.getElementById("img").src="https://images.vexels.com/media/users/3/131398/isolated/lists/f9184d3dae2693f076c58f43569f6d0e-man-running-sequence-4.png"
+}
+
+function jump2(){
+    document.getElementById("img").src="https://images.vexels.com/media/users/3/131400/isolated/lists/d0c3950c91fbadc2a7c49ead507cee4c-man-running-sequence-6.png" 
+}
+
+function jump3(){
+    document.getElementById("img").src="https://images.vexels.com/media/users/3/131368/isolated/lists/20ae7c29294db9e5f9a1b368aec7a028-man-jumping-sequence-4.png"
+}
+
+
+function stop(){
+    document.getElementById("img").src="https://images.vexels.com/media/users/3/131359/isolated/lists/538629332d567c00ddb3cf233862e78d-man-jumping-sequence-1.png"
+}
+
+function dog1(){
+    document.getElementById("img2").src="https://images.vexels.com/media/users/3/130106/isolated/preview/d62201989552ef4beacf49d9b5a7e1a3-dog-running-sequence-4-by-vexels.png"
+}
+
+function dog2(){
+    document.getElementById("img2").src="https://images.vexels.com/media/users/3/130105/isolated/preview/5067de1c837add0f07a6587cc192f03c-dog-running-sequence-3.png"   
+}
+
+
+function dog3(){
+    document.getElementById("img2").src="https://images.vexels.com/media/users/3/130108/isolated/preview/3b1a629dec7fa1e3dfdd84417014c235-dog-running-sequence-6-by-vexels.png"
+}
